db7f39ca6d7c9e6ec901f119a667dc60b76a0930
Anuvind Venu Menon <manuvind@gmail.com>
2014-03-28 22:49:48 -0700

    empty rails app for doorbell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doorbell::Application.config.secret_token = 'a733fb1701e0a7e9f3a7b96541c7344d2dea6258efa605c147ad6d5a98237a9a0894ce0248213317a049e3a63d2c5a5f9b6c15352e467da99ea4f5b772bec773'
