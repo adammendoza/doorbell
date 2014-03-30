353ced65aeb12012020d2428e18a4f700791544e
Anuvind Venu Menon <manuvind@gmail.com>
2014-03-30 12:53:13 -0700

    add ability for multiple users to get text msgs

--- app/controllers/doorbells_controller.rb
+++ app/controllers/doorbells_controller.rb
@@ -1,9 +1,11 @@
 class DoorbellsController < ApplicationController
 	def test
-		number = params['num']
-		message = params['msg']
-		blowerio = RestClient::Resource.new(ENV['BLOWERIO_URL'])
-		blowerio['/messages'].post :to => "+1#{number}", :message => "#{message}"
+		User.where(:contact => true).each do |u|
+			num = u.number
+			msg = u.msg
+			blowerio = RestClient::Resource.new(ENV['BLOWERIO_URL'])
+			blowerio['/messages'].post :to => "+1#{num}", :message => "#{msg}"
+		end
 		redirect_to root_url
 	end
 end
